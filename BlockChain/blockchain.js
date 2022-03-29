61b256d9b9538bc513e5c566683c1b3eff052a39
juhyeon <forbestlife03@gmail.com>
2022-03-29 13:42:08 +0900

    add proofOfWorking module

--- BlockChain/blockchain.js
+++ BlockChain/blockchain.js
@@ -1,8 +1,11 @@
+const sha256 = require('sha256')
+
 function BlockChain(){
     this.chain = [];
     this.pendingTransaction = [];
+    this.createNewBlock(100,'0','0')
 }
-
+//New block function
 BlockChain.prototype.createNewBlock = function(nonce, previousBlockHash, hash){
     const newBlock = {
         index: this.chain.length +1,
@@ -17,4 +20,39 @@ BlockChain.prototype.createNewBlock = function(nonce, previousBlockHash, hash){
     return newBlock
 }
 
+BlockChain.prototype.getLastBlock =  function() {
+    
+    return this.chain[this.chain.length-1];
+}
+//New transaction fucntion
+BlockChain.prototype.createNewTransaction = function(amount, sender,recipient){
+    const newTransaction = {
+        amount : amount,
+        sender : sender,
+        recipient : recipient
+    }
+    this.pendingTransaction.push(newTransaction)
+    return this.getLastBlock()['index'] + 1
+}
+BlockChain.prototype.hashBlock = function (previousBlockHash, currentTransactions, nonce){
+    const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentTransactions)
+    return sha256(dataAsString)
+}
+
+BlockChain.prototype.proofOfWork = function(previousBlockHash, cunrrentBlockData){
+    this.difficulty = 3
+    this.nonce = 0
+    const target = '0'.repeat(this.difficulty)
+    while(true){
+        var currentHash  = this.hashBlock(previousBlockHash,cunrrentBlockData,this.nonce)
+        console.log(`nonce : ${this.nonce} | hash : ${currentHash}`)
+        if (target == currentHash.substring(0,this.difficulty)){
+            return this.nonce}
+        
+        else{
+            this.nonce++
+        } 
+    }
+}
+// console.log(sha256("abc123").substring(0,1))
 module.exports = BlockChain;
--- BlockChain/test.js
+++ BlockChain/test.js
@@ -3,8 +3,21 @@ const BlockChain = require('./blockchain')
 const bitcoin = new BlockChain();
 //create new blocks
 
-bitcoin.createNewBlock(1111,"previous Hash","hash1")
-bitcoin.createNewBlock(2222,"hash1","hash2")
-bitcoin.createNewBlock(3333,"hash2","hash3")
+// bitcoin.createNewBlock(1111,"previous Hash","hash1")
+// bitcoin.createNewBlock(2222,"hash1","hash2")
+// bitcoin.createNewBlock(3333,"hash2","hash3")
+// bitcoin.createNewTransaction(100, "kim","lee")
+// bitcoin.createNewTransaction(200, "kim2","lee2")
+// bitcoin.createNewTransaction(300, "kim3","lee3")
+// bitcoin.createNewBlock(4444, "hash3", "hash4")
+// console.log(bitcoin);
+// console.log(bitcoin.getLastBlock())
 
-console.log(bitcoin);
+const previousBlockHash = "abc"
+const currentTransactions = [ 
+    { amount: 100, sender: 'kim', recipient: 'lee' },
+    { amount: 200, sender: 'kim1', recipient: 'lee2' },
+    { amount: 300, sender: 'kim3', recipient: 'lee3' }
+]
+
+console.log(bitcoin.proofOfWork(previousBlockHash,currentTransactions))
